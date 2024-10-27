7d7de3e0446b3595ec871e594626d60c4344f677
Eduardo B. Alexandre <sezdocs@github.com>
2024-10-27 16:49:53 -0300

    Remove some comments

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration of sezdocs";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
